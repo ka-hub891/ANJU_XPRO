15810be9bbf1369745992429659703c70467dba3
ka-hub891 <silencekkavishan@gmail.com>
2025-04-14 15:58:02 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,9 +40,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~GR9gzKzR#fQuE850Dpj1HbSfMok3cl-B7SJ23Qxt3iu9iNx57IQg",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "ka-hub891",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
